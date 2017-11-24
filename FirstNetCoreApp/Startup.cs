83014a19fa42a15391d61a675b8cec1524a0da3b
popovsli <popovsli91@gmail.com>
2017-11-24 17:40:57 +0200

    add dependancy injection

--- FirstNetCoreApp/Startup.cs
+++ FirstNetCoreApp/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FirstNetCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstNetCoreApp
 {
@@ -21,6 +23,11 @@ namespace FirstNetCoreApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires 
+            // using RazorPagesMovie.Models;
+            // using Microsoft.EntityFrameworkCore;
+            services.AddDbContext<MovieContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
