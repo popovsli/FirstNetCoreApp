449a7a19ded0e4c80f3d6fa460b61a918f58303a
popovsli <popovsli91@gmail.com>
2017-12-12 17:56:22 +0200

    Add new model class

---
+++ BusinessEntities/Models/UserBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessEntities.Models
+{
+    public class UserBase
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
