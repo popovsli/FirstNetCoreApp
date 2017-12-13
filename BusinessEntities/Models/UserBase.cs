de7554b0881c3b67bada7211eadb5a6cc428bd2d
popovsli <popovsli91@gmail.com>
2017-12-13 17:38:11 +0200

    Add factory pathern

--- BusinessEntities/Models/UserBase.cs
+++ BusinessEntities/Models/UserBase.cs
@@ -9,5 +9,11 @@ namespace BusinessEntities.Models
         public string UserName { get; set; }
 
         public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
     }
 }
---
+++ BusinessLayer/Factory/Factory.cs
@@ -0,0 +1,25 @@
+﻿using BusinessEntities.Models;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Factory
+{
+    public static class Factory<ReturnType>
+    {
+
+        static IUnityContainer container = null;
+
+        static Factory()
+        {
+            //container.RegisterType<ReturnType, Movie>("Movie");
+        }
+
+        public static ReturnType Create<InstanceType>(Type instanceType)
+        {
+
+            return container.Resolve<ReturnType>(instanceType.Name);
+        }
+    }
+}
