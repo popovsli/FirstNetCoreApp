ed7e00d444bd676caaf04a694a22af1669b26e92
popovsli <popovsli91@gmail.com>
2018-01-22 17:46:51 +0200

    test

--- BusinessEntities/Context/MovieContext.cs
+++ BusinessEntities/Context/MovieContext.cs
@@ -7,6 +7,11 @@ namespace BusinessEntities.Context
 {
     public class MovieContext : DbContext
     {
+        //Add migration- Add-Migration NewMigration -Project "Project name"
+        //Create the database and tables in it- Update-Database
+        //To delete the last migration that you added -Remove-Migration
+        //Reverting a migration- Update-Database LastGoodMigration
+
         public MovieContext(DbContextOptions<MovieContext> options)
                 : base(options)
         {
@@ -26,8 +31,8 @@ namespace BusinessEntities.Context
 
         //protected override void OnModelCreating(ModelBuilder modelBuilder)
         //{
-        //    modelBuilder.Entity<Movie>().ToTable(typeof(Movie).Name);
-        //    modelBuilder.Entity<Schedule>().ToTable(typeof(Schedule).Name);
+        //    modelBuilder.Entity<Movie>().ToTable(nameof(Movie));
+        //    modelBuilder.Entity<Schedule>().ToTable(nameof(Schedule));
 
         //    base.OnModelCreating(modelBuilder);
         //}
